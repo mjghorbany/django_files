3176abb3e905f9de9aae7f0474ad67aae53fb076
Jay <jay.ghorbani@accenture.com>
2017-07-24 21:36:16 -0700

    new stuff with query

--- mysite/personal/models.py
+++ mysite/personal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Triple(models.Model):
+    node1=models.CharField(max_length=20)
+    edge=models.TextField()
+    node2=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.node1
--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -4,15 +4,17 @@ from django.http import HttpResponse
 import requests
 from py2neo import Graph,authenticate
 import json
+from .models import Triple
 
 
-
-def get_books():
+def get_books(claim):
     authenticate("localhost:7474", "neo4j", "mjgh2765")
     graph = Graph()
-    query = """
-    MATCH (n:Drink) RETURN n LIMIT 3
-    """
+
+    #query="""MATCH (p:Person { name: 'Sally' }) RETURN p LIMIT 25"""
+    query_before = """MATCH (record:"""
+    query_after = """) RETURN record LIMIT 6"""
+    query = query_before + claim + query_after
     data = graph.cypher.execute(query)
 
 
@@ -27,8 +29,26 @@ def get_books():
 
 # Create your views here.
 def index(request):
-    books=get_books()
-    return render(request,'personal/graph1.html',{'books': books})
+    return render(request,'personal/graph1.html')
+
+
+def search(request):
+    if request.method == 'POST':
+        print('in here')
+        claim = request.POST.get('claim_field', None)
+        print('claim_id: ',claim)
+        try:
+            #claim = Triple.objects.get(node1 = search_id)
+            #do something with user
+            books = get_books(claim)
+            print(books)
+            return render(request,'personal/graph1.html',{'books': books})
+        except Triple.DoesNotExist:
+            print('no match')
+            return HttpResponse("Error")
+
+    else:
+        return render(request, 'graph1.html')
 
 
 def contact(request):
